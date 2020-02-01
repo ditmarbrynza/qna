a1ecea7974df575244585da4dbdba0400ca5ba6c
ditmarbrynza <ditmarwork@gmail.com>
2020-02-01 23:24:30 +0300

    add answer

---
+++ app/models/answer.rb
@@ -0,0 +1,8 @@
+class Answer < ApplicationRecord
+
+  belongs_to :question
+
+  validates :body, presence: true
+  validates :question_id, presence: true
+
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
+
+  has_many :answers
+
   validates :title, :body, presence: true
+
 end
