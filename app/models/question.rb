1e1b617aad24ce65625dc70fdc18ef97972f5b31
ditmar <ditmarwork@gmail.com>
2020-10-03 19:58:28 +0300

    add attaching one file

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, -> { order(best: :desc) }, dependent: :destroy
   belongs_to :user
+  has_one_attached :file
   
   validates :title, :body, presence: true
 end
