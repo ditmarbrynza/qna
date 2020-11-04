9ef3a0488894fe562dbd75e53b3255119f88c341
ditmar <ditmarwork@gmail.com>
2020-11-04 04:44:07 +0300

    add fetching gists with js

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,16 +1,7 @@
 class Link < ApplicationRecord
-  GISTS_REGEX = /https:\/\/gist.github.com\//
-
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
   validates :url, format: URI::regexp(%w[http https])
 
-  def gist_url?
-    url.match?(GISTS_REGEX)
-  end
-
-  def gist_url
-    Octokit::Client.new.gist(url.split('/').last).files.map { |f| { name: f[0].to_s, content: f[1]['content'] } }
-  end
 end
