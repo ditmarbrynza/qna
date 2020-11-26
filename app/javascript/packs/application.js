2e14d7f53ee21d27b9e0d1622903f165d384e0fa
ditmar <ditmarwork@gmail.com>
2020-11-27 00:57:35 +0300

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ require("answers/edit")
 require("questions/edit")
 require("jquery")
 require("@nathanvda/cocoon")
-require("links/fetchGist")
+require("links/renderGist")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
