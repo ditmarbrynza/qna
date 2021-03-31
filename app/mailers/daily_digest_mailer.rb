c24ff1cd9e0ca984f66ac803ecb9c40c9a439bd3
ditmar <ditmarwork@gmail.com>
2021-03-31 20:52:37 +0300

    add some fixes

--- app/mailers/daily_digest_mailer.rb
+++ app/mailers/daily_digest_mailer.rb
@@ -3,6 +3,7 @@
 class DailyDigestMailer < ApplicationMailer
   def digest(user)
     @questions = Question.where(created_at: (Time.now.midnight - 1.day)..Time.now.midnight)
+    return if @questions.empty?
 
     mail to: user.email
   end
