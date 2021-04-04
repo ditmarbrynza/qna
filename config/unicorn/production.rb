449b918e5e0009d288046d6b01d061a6b742e1a3
ditmar <ditmarwork@gmail.com>
2021-04-04 04:14:29 +0300

    fix code style

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -35,7 +35,7 @@ before_fork do |server, _worker|
     begin
       Process.kill('QUIT', File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
+      puts "[#{self.class}] exeption"
     end
   end
 end
