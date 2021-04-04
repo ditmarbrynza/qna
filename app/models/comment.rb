3809e4837e314a210b6d232ec0a74c139fa3f928
ditmar <ditmarwork@gmail.com>
2021-04-04 04:01:30 +0300

    add cache

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@
 
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
 
   validates :text, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module Qna
                         request_specs: false,
                         controller_specs: true
     end
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache/', { expires_in: 90.minutes }
   end
 end
